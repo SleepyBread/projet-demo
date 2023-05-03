3f8cd2229e90dc905ac122d107f346ab1468415a
SleepyBread <jnroy27@gmail.com>
2023-05-03 16:49:22 -0400

    Added test form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestFormComponent } from './test-form/test-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/test-form/test-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-form',
+  templateUrl: './test-form.component.html',
+  styleUrls: ['./test-form.component.css']
+})
+export class TestFormComponent {
+  submitForm(): void {
+    console.log("Form submitted");
+  }
+}
