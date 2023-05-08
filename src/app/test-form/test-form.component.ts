a201713b298e873efd62fb5feb4223d1d6c834fa
SleepyBread <jnroy27@gmail.com>
2023-05-08 10:45:09 -0400

    Add smell to code

--- src/app/test-form/test-form.component.ts
+++ src/app/test-form/test-form.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-test-form',
   templateUrl: './test-form.component.html'
 })
 export class TestFormComponent {
+  @ViewChild('lastName') lastName!: ElementRef<HTMLInputElement>;
+  @ViewChild('firstName') firstName!: ElementRef<HTMLInputElement>;
+
   submitForm(): void {
-    console.log("Form submitted");
+    let strName = this.firstName.nativeElement.value + ' ' + this.lastName.nativeElement.value;
+
+    console.log(strName);
   }
 }
