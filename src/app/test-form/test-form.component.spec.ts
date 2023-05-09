3da1bdee682f6b9ccadf5ce980a661b6e066a1e4
SleepyBread <jnroy27@gmail.com>
2023-05-09 16:29:04 -0400

    Add unit tests

--- src/app/test-form/test-form.component.spec.ts
+++ src/app/test-form/test-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { TestFormComponent } from './test-form.component';
@@ -21,5 +21,37 @@ describe('TestFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when submit button is pressed', () => {
+    it('should submit form when fields are filled', fakeAsync(() => {
+      component.nameForm.setValue({
+        firstName: 'firstName',
+        lastName: 'lastName'
+      })
+
+      spyOn(window, 'alert');
+
+      let button = fixture.debugElement.nativeElement.querySelector('#submitButton');
+      button.click();
+      tick();
+
+      expect(window.alert).toHaveBeenCalled();
+    }));
+
+    it('should not submit form when a field is empty', fakeAsync(() => {
+      component.nameForm.setValue({
+        firstName: 'firstName',
+        lastName: ''
+      })
+
+      spyOn(window, 'alert');
+
+      let button = fixture.debugElement.nativeElement.querySelector('#submitButton');
+      button.click();
+      tick();
+
+      expect(window.alert).not.toHaveBeenCalled();
+    }));
   });
 });
